51f4bcf46f61c685372461f4d69df2b528d90274
matieuruth <matieu.ruth@gmail.com>
2020-09-24 22:58:21 -0400

    update skills section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ function Nav() {
 
     return (
       <nav>
-        <div className="nav-item">
+        <div className="nav-item nav-logo">
           <p className="logo">Alanna Mathieu</p>
         </div>
         <div className="nav-item">
