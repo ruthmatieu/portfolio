10e5f58648b2d24e745dd739ef898ac9ff552fa7
matieuruth <matieu.ruth@gmail.com>
2021-01-09 22:02:11 -0500

    update project info

--- src/components/Projects/Lavabeans.js
+++ src/components/Projects/Lavabeans.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from '../Nav';
+import '../../styles/Lavabeans.css';
 
 
 //images
@@ -25,29 +26,29 @@ const Lavabeans = () => {
         <div>
             <Nav/>
             
-            <div style={{textAlign: 'center'}}>
-                <h1 style={{fontSize: '3rem', padding: '50px 0'}}>Lavabeans Coffee Co.</h1>
-                <p style={{padding: '0 250px', textAlign: 'left'}}>Introduction (few sentences that explains that the app is about).<br/>
+            <div className='project-wrapper'>
+                <h1>Lavabeans Coffee Co.</h1>
+                <p>Introduction (few sentences that explains that the app is about).<br/>
                     Lavabeans Coffee is a React Application built for a self roasting coffee kit. I built this project from scratch.
                     Introduction (few sentences that explains that the app is about).
                     Lavabeans Coffee is a React Application built for a self roasting coffee kit. I built this project from scratch.
                 </p>
                 
-                <div style={{display: 'flex', justifyContent:'space-around' , padding: '50px 250px'}}>
+                <div className='tech-stack'>
                     <div>
                         <p>Tech stack:<br/> React, Redux, CSS, HTML5, Styled Components</p>
                     </div>
                     <div>
-                        <p><a style={{color: 'pink', textDecoration: 'none', padding: '0 5px'}} href={data.link} target='_blank' rel="noopener noreferrer">View project</a></p>
-                        <p><a style={{color: 'pink', textDecoration: 'none', padding: '0 5px'}} href={data.github} target='_blank' rel="noopener noreferrer">View GitHub</a></p>
+                        <p className='link'><a style={{color: 'pink', textDecoration: 'none'}} href={data.link} target='_blank' rel="noopener noreferrer">View project</a></p>
+                        <p className='link'><a style={{color: 'pink', textDecoration: 'none'}} href={data.github} target='_blank' rel="noopener noreferrer">View GitHub</a></p>
                     </div>
                 </div>
-                <img style={{maxWidth: '60%', boxShadow: '0px 0px 33px 16px rgba(243,171,129,0.40)'}} src={homepage} alt='homepage'/>
+                <img src={homepage} alt='homepage' className='project-homepage-screenshot'/>
             </div>
 
-            <div style={{textAlign: 'left', paddingLeft: '250px'}}>
-                <h3 style={{fontSize: '2rem', padding: '150px 0 50px 0'}}>Purpose + Goal</h3>
-                <div style={{padding: '0 250px 80px 0'}}>
+            <div className='project-details'>
+                <h3>Purpose + Goal</h3>
+                <div className='details'>
                     <p style={{paddingBottom: '20px'}}>This project included 3 phases. There are many variations of passages of Lorem Ipsum available, but the majority 
                         have suffered alteration in some form, by injected humour, or randomised words 
                         which don't look even slightly believable. If you are going to use a passage of 
@@ -68,12 +69,12 @@ const Lavabeans = () => {
                         The generated Lorem Ipsum is therefore always free from repetition, injected 
                         humour, or non-characteristic words etc.</p>
                 </div>
-                <img style={{maxWidth: '60%', boxShadow: '0px 0px 33px 16px rgba(243,171,129,0.20)'}} src={snippetOne} alt='project screenshot'/>
+                <img src={snippetOne} alt='project screenshot'/>
             </div>
 
-            <div style={{textAlign: 'right', paddingRight: '250px'}}>
-                <h3 style={{fontSize: '2rem', padding: '150px 0 50px 0'}}>Spotlight</h3>
-                <div style={{padding: '0 0 80px 250px'}}>
+            <div className='project-details'>
+                <h3>Spotlight</h3>
+                <div className='details'>
                     <p>There are many variations of passages of Lorem Ipsum available, but the majority 
                         have suffered alteration in some form, by injected humour, or randomised words 
                         which don't look even slightly believable. If you are going to use a passage of 
@@ -86,18 +87,18 @@ const Lavabeans = () => {
                         humour, or non-characteristic words etc.</p>
                 </div>
                 <div>
-                    <img style={{maxWidth: '800px',  marginBottom: '50px', boxShadow: '0px 0px 33px 16px rgba(243,171,129,0.20)'}} src={spotlightThree} alt='project screenshot'/>
+                    <img style={{maxWidth: '800px',  marginBottom: '50px'}} src={spotlightThree} alt='project screenshot'/>
                     <div style={{paddingBottom: '150px', display: 'flex', justifyContent: 'flex-end', flexWrap: 'wrap'}}>
-                        <img style={{width: '500px', boxShadow: '0px 0px 33px 16px rgba(243,171,129,0.20)'}} src={spotlightOne} alt='project screenshot'/>
-                        <img style={{width: '600px', boxShadow: '0px 0px 33px 16px rgba(243,171,129,0.20)'}} src={spotlightTwo} alt='project screenshot'/>
+                        <img src={spotlightOne} alt='project screenshot'/>
+                        <img src={spotlightTwo} alt='project screenshot'/>
                     </div>
                 </div>
                 
             </div>
 
-            <div style={{textAlign: 'center'}}>
-                <h3 style={{fontSize: '2rem', padding: '0 0 50px 0'}}>Problems + Lessons Learned</h3>
-                <div style={{padding: '0 250px', textAlign: 'left'}}>
+            <div className='project-details'>
+                <h3>Problems + Lessons Learned</h3>
+                <div className='details'>
                     <div>
                     <p style={{paddingBottom: '20px'}}>This project included 3 phases. There are many variations of passages of Lorem Ipsum available, but the majority 
                         have suffered alteration in some form, by injected humour, or randomised words 
