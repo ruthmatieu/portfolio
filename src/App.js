96570c930cbab355ba0cc766c9e2d20e8cfad83d
Alanna Matieu <alannamatieu@rmatieu-mac.lan>
2023-03-25 16:38:31 -0400

    removed arrow

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ function App() {
         <Contact/>
         <Footer/>
       </div>
-      <div className="absolute" style={{backgroundColor: "#F5F5EC", bottom: "30%"}} >
+      {/* <div className="absolute" style={{backgroundColor: "#F5F5EC", bottom: "30%"}} >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M13 7.828V20h-2V7.828l-5.364 5.364-1.414-1.414L12 4l7.778 7.778-1.414 1.414L13 7.828z"/></svg>
         <p className={backToTop ? "show-arrow" : "hide-arrow"}>Back to top</p>
-      </div>
+      </div> */}
     </div>
   );
 }
