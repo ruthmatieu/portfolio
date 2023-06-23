e634f4d588233c5e48d4341e61cb993fab0ab00e
Joey Trujillo-Bini <joeytrujillo-bini@MacBook-Pro-2.lan>
2023-06-23 11:27:13 -0400

    redesign, refactored, and rebuilt porfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px'
-    },
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
